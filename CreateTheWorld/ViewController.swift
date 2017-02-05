6818cc7f375a04636d0bfeb25d007dd251d661e7
Jason Christians <jasondchristians@icloud.com>
2017-02-05 15:10:46 -0700

    Added XCode Project

---
+++ CreateTheWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CreateTheWorld
+//
+//  Created by Jason Christians on 2/5/17.
+//  Copyright © 2017 Jason Christians. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
